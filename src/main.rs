96bd941cb6267fd2df08c63143b601f874146e64
ibraheemdev <ibrah1440@gmail.com>
2021-04-13 16:51:44 -0400

    basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::io::{self, BufRead, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = std::env::args().skip(1);
+
+    if args.len() > 1 {
+        println!("Usage: eunoia [script]");
+        std::process::exit(64);
+    }
+
+    match args.next() {
+        Some(path) => {
+            run_file(path.as_ref()).unwrap();
+        }
+        None => {
+            repl();
+        }
+    }
+}
+
+fn run_file(path: &str) -> Result<(), io::Error> {
+    let file = std::fs::read_to_string(path)?;
+    run(file.as_ref());
+    Ok(())
+}
+
+fn run(source: &str) {
+    print!("{}", source);
+}
+
+fn repl() {
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+
+        let mut line = String::new();
+        match handle.read_line(&mut line) {
+            Ok(_) => run(line.as_ref()),
+            Err(_) => break,
+        }
+    }
 }
